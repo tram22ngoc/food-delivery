bb447b4848f740d9810c76549004885a5b3bd54a
GiaPhong21001194 <135103584+GiaPhong21001194@users.noreply.github.com>
2023-09-24 00:04:36 +0700

    Update Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,16 +1,62 @@
-import React from 'react';
-import Helmet from '../components/Helmet/Helmet';
-import CommonSection from '../components/UI/common-section/CommonSection';
-import { Container, Row, Col } from 'reactstrap';
+import React from "react";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/common-section/CommonSection";
+import { Container, Row, Col } from "reactstrap";
 
 const Contact = () => {
   return (
-    <Helmet title='Contact'>
-      <CommonSection title='Contact' />
+    <Helmet title="Contact">
+      <CommonSection title="Contact" />
       <section>
         <Container>
           <Row>
-            <Col></Col>
+            <Col>
+              <h2>Liên Hệ</h2>
+              <p>
+                Để liên hệ với chúng tôi hoặc theo dõi chúng tôi trên các mạng
+                xã hội, bạn có thể sử dụng các liên kết sau:
+              </p>
+              <ul>
+                <li>
+                  <a
+                    href="https://www.facebook.com/foodie"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Facebook
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.instagram.com/foodie/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Instagram
+                  </a>
+                </li>
+              </ul>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h2>Địa Chỉ Cửa Hàng</h2>
+              <ul>
+                <li>CN I: 445 Sư Vạn Hạnh, P.12, Q.10, TP.HCM.</li>
+                <li>CN II: 48 Trần Quang Diệu, P.14, Q.3, TP.HCM.</li>
+                <li>CN III: 350 Điện Biên Phủ, P.17, Q. Bình Thạnh, TP.HCM.</li>
+                <li>
+                  CN IV: G-Town 2, 136 Nguyễn Hồng Đào, P.14, Q.Tân Bình,
+                  TP.HCM.
+                </li>
+                <li>CN V: 463 Quang Trung, P.10, Q. Gò Vấp, TP.HCM.</li>
+                <li>CN VI: TNP - 26 Lý Tự Trọng, P. Bến Nghé, Q.1, TP.HCM.</li>
+                <li>
+                  CN VII: TNP (Sense Market) - Đối Diện Số 90 Lê Lai, P. Bến
+                  Nghé, Q.1, TP.HCM.
+                </li>
+              </ul>
+            </Col>
           </Row>
         </Container>
       </section>
