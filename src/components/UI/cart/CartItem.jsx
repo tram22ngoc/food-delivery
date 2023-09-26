15d1484a3571393915f5ef5477a1b0c4238a15d1
tram22ngoc <ngoctram240903@gmail.com>
2023-09-26 12:28:01 +0700

    test product and add cart

--- src/components/UI/cart/CartItem.jsx
+++ src/components/UI/cart/CartItem.jsx
@@ -1,46 +1,46 @@
-import React from 'react';
-import { ListGroupItem } from 'reactstrap';
-import { useDispatch } from 'react-redux';
-import { cartActions } from '../../../store/shopping-cart/cartSlice';
-import '../../../styles/cart-item.css';
+import React from "react";
+import { ListGroupItem } from "reactstrap";
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../../store/shopping-cart/cartSlice";
+import "../../../styles/cart-item.css";
 const CartItem = ({ item }) => {
-  const { id, title, price, image01, quantity, totalPrice } = item;
+  const { id, name, price, image01, quantity, totalPrice } = item;
   const dispatch = useDispatch();
   // Hàm 'increaseItem' để tăng số lượng của một sản phẩm trong giỏ hàng
   const increaseItem = () => {
-    dispatch(cartActions.addItem({ id, title, price, image01 }));
+    dispatch(cartActions.addItem({ id, name, price, image01 }));
   };
   // Hàm 'increaseItem' để giảm số lượng của một sản phẩm trong giỏ hàng
   const decreaseItem = () => {
     dispatch(cartActions.removeItem(id));
   };
-    // Hàm 'deleteItem' để xóa một sản phẩm khỏi giỏ hàng
+  // Hàm 'deleteItem' để xóa một sản phẩm khỏi giỏ hàng
   const deleteItem = () => {
     dispatch(cartActions.deleteItem(id));
   };
-   // Giao diện của một mục trong giỏ hàng
+  // Giao diện của một mục trong giỏ hàng
   return (
-    <ListGroupItem className='border-0 cart__item' key={id}>
-      <div className='cart__item-info d-flex gap-2'>
-        <img src={image01} alt='product-img' />
-        <div className='cart__product-info w-100 d-flex align-items-center justify-content-between gap-4'>
+    <ListGroupItem className="border-0 cart__item" key={id}>
+      <div className="cart__item-info d-flex gap-2">
+        <img src={image01} alt="product-img" />
+        <div className="cart__product-info w-100 d-flex align-items-center justify-content-between gap-4">
           <div>
-            <h6 className='cart__product-title'>{title}</h6>
-            <p className='d-flex align-items-center gap-5 cart__product-price'>
+            <h6 className="cart__product-title">{name}</h6>
+            <p className="d-flex align-items-center gap-5 cart__product-price">
               x {quantity} = <span>${totalPrice}</span>
             </p>
-            <div className='d-flex align-items-center justify-content-between increase__decrease-btn'>
-              <span onClick={increaseItem} className='increase__btn'>
-                <i className='ri-add-line' />
+            <div className="d-flex align-items-center justify-content-between increase__decrease-btn">
+              <span onClick={increaseItem} className="increase__btn">
+                <i className="ri-add-line" />
               </span>
-              <span className='quantity'>{quantity}</span>
-              <span onClick={decreaseItem} className='decrease__btn'>
-                <i className='ri-subtract-line' />
+              <span className="quantity">{quantity}</span>
+              <span onClick={decreaseItem} className="decrease__btn">
+                <i className="ri-subtract-line" />
               </span>
             </div>
           </div>
-          <span className='delete__btn' onClick={deleteItem}>
-            <i className='ri-close-line' />
+          <span className="delete__btn" onClick={deleteItem}>
+            <i className="ri-close-line" />
           </span>
         </div>
       </div>
--- src/components/UI/product-card/ProductCard.jsx
+++ src/components/UI/product-card/ProductCard.jsx
@@ -1,37 +1,56 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../../../styles/product-card.css';
-import { useDispatch } from 'react-redux';
-import { cartActions } from '../../../store/shopping-cart/cartSlice';
-const ProductCard = (props) => {
-  const { id, title, image01, price } = props.item;
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import "../../../styles/product-card.css";
+import { useDispatch, useSelector } from "react-redux";
+import { cartActions } from "../../../store/shopping-cart/cartSlice";
+import { fetchData } from "../../../store/admin/productsSlice";
+const ProductCard = () => {
   const dispatch = useDispatch();
-  const addToCart = () => {
+  const products = useSelector((state) => state.product.products);
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
+
+  const addToCart = (id, name, price, image1) => {
     dispatch(
       cartActions.addItem({
         id,
-        title,
-        image01,
+        name,
         price,
+        image1,
       })
     );
   };
   return (
-    <div className='product__item'>
-      <div className='product__img'>
-        <img src={image01} alt='product-img' className='w-50' />
-      </div>
-      <div className='product__content'>
-        <h5>
-          <Link to={`/foods/${id}`}>{title}</Link>
-        </h5>
-        <div className='d-flex align-items-center justify-content-between'>
-          <span className='product__price'>${price}</span>
-          <button onClick={addToCart} className='addToCart__btn'>
-            Add to cart
-          </button>
+    <div>
+      {products.map((product) => (
+        <div className="product__item" key={product.id}>
+          <div className="product__img">
+            <img src={product.image1} alt="product-img" className="w-50" />
+          </div>
+          <div className="product__content">
+            <h5>
+              <Link to={`/foods/${product.id}`}>{product.name}</Link>
+            </h5>
+            <div className="d-flex align-items-center justify-content-between">
+              <span className="product__price">${product.price}</span>
+              <button
+                onClick={() =>
+                  addToCart(
+                    product.id,
+                    product.name,
+                    product.price,
+                    product.image1
+                  )
+                }
+                className="addToCart__btn"
+              >
+                Add to cart
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
