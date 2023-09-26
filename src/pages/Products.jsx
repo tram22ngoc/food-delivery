25f8db59cf70be2e6326cf771122d2b29af1bc8a
tram22ngoc <ngoctram240903@gmail.com>
2023-09-26 12:06:32 +0700

    fix crud products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Menu from "../components/Admin/Menu";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle";
@@ -7,6 +7,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { deleteProduct, fetchData } from "../store/admin/productsSlice";
 
 const Products = () => {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
   const dispatch = useDispatch();
   const products = useSelector((state) => state.product.products);
   const navigate = useNavigate();
@@ -48,11 +50,13 @@ const Products = () => {
                 <select
                   className="form-select"
                   aria-label="Default select example"
+                  value={selectedCategory}
+                  onChange={(e) => setSelectedCategory(e.target.value)}
                 >
-                  <option selected>Select category</option>
-                  <option value={1}>Burger</option>
-                  <option value={2}>Pizza</option>
-                  <option value={3}>Bread</option>
+                  <option value="All">All</option>
+                  <option value="Burger">Burger</option>
+                  <option value="Pizza">Pizza</option>
+                  <option value="Bread">Bread</option>
                 </select>
               </div>
             </div>
@@ -78,34 +82,65 @@ const Products = () => {
 
               <tbody>
                 {Array.isArray(products) && products.length > 0 ? (
-                  products.map((product) => (
-                    <tr key={product.id}>
-                      <td>{product.name}</td>
-                      <td>{product.category}</td>
-                      <td>${product.price}</td>
-                      <td>{product.desc}</td>
-                      <td>
-                        <div>
-                          {/* Button trigger modal */}
-                          <a
-                            type="button"
-                            className="text-warning"
-                            onClick={(e) => showEditPane(null, product.id)}
-                          >
-                            <i class="fa-solid fa-pen"></i>
-                          </a>
-                          <span> </span>
-                          <i
-                            className="fa-solid fa-remove text-danger"
-                            onClick={() => handleDeleteProduct(product.id)}
-                          ></i>
-                        </div>
-                      </td>
-                    </tr>
-                  ))
+                  products
+                    .filter((product) => {
+                      if (selectedCategory === "All") {
+                        return true; // Show all products if no category is selected
+                      } else {
+                        return product.category === selectedCategory;
+                      }
+                    })
+                    .map((product) => (
+                      <tr key={product.id}>
+                        <td>{product.name}</td>
+                        <td>{product.category}</td>
+                        <td>${product.price}</td>
+                        <td>
+                          <div className="flex">
+                            <img
+                              src={product.image1}
+                              alt=""
+                              width="100px"
+                              height="100px"
+                            />
+                            <img
+                              src={product.image2}
+                              alt=""
+                              className="mx-2"
+                              width="100px"
+                              height="100px"
+                            />
+                            <img
+                              src={product.image3}
+                              alt=""
+                              width="100px"
+                              height="100px"
+                            />
+                          </div>
+                        </td>
+                        <td>{product.desc}</td>
+                        <td>
+                          <div>
+                            {/* Button trigger modal */}
+                            <a
+                              type="button"
+                              className="text-warning"
+                              onClick={(e) => showEditPane(null, product.id)}
+                            >
+                              <i class="fa-solid fa-pen"></i>
+                            </a>
+                            <span> </span>
+                            <i
+                              className="fa-solid fa-remove text-danger"
+                              onClick={() => handleDeleteProduct(product.id)}
+                            ></i>
+                          </div>
+                        </td>
+                      </tr>
+                    ))
                 ) : (
                   <tr>
-                    <td colSpan="3">Loading...</td>
+                    <td colSpan="6">No products found.</td>
                   </tr>
                 )}
               </tbody>
