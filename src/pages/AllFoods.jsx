27bb9c986b974083b6b42b80f0cc8f9f3ce34615
GiaPhong21001194 <135103584+GiaPhong21001194@users.noreply.github.com>
2023-09-27 14:44:19 +0700

    fix allfoods

--- src/pages/AllFoods.jsx
+++ src/pages/AllFoods.jsx
@@ -1,37 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CommonSection from "../components/UI/common-section/CommonSection";
 import Helmet from "../components/Helmet/Helmet";
 import { Container, Row, Col } from "reactstrap";
-import products from "../assets/fake-data/products";
 import ProductCard from "../components/UI/product-card/ProductCard";
 import "../styles/all-foods.css";
-import ReactPaginate from "react-paginate";
 import "../styles/pagination.css";
 
+import { useDispatch, useSelector } from "react-redux";
+import { fetchData } from "../store/admin/productsSlice";
 const AllFoods = () => {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [pageNumber, setPageNumber] = useState(0);
-
-  const searchedProduct = products.filter((item) => {
-    if (searchTerm === "") {
-      return true; // Trả về true để giữ lại tất cả các phần tử khi searchTerm trống
-    } else if (item.title.toLowerCase().includes(searchTerm.toLowerCase())) {
-      return true; // Trả về true để giữ lại phần tử thỏa mãn điều kiện tìm kiếm
-    }
-    return false; // Trả về false để loại bỏ các phần tử không thỏa mãn điều kiện
-  });
-
-  const productPerPage = 8;
-  const visitedPage = pageNumber * productPerPage;
-  const displayPage = searchedProduct.slice(
-    visitedPage,
-    visitedPage + productPerPage
-  );
-
-  const pageCount = Math.ceil(searchedProduct.length / productPerPage);
-  const changePage = ({ selected }) => {
-    setPageNumber(selected);
-  };
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.product.products);
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
 
   return (
     <Helmet title="All Foods">
@@ -39,42 +21,10 @@ const AllFoods = () => {
       <section>
         <Container>
           <Row>
-            <Col lg="6" md="6" sm="6" xs="12">
-              <div className="search__widget d-flex align-items-center justify-content-between">
-                <input
-                  type="text"
-                  placeholder="Search..."
-                  value={searchTerm}
-                  onChange={(e) => setSearchTerm(e.target.value)}
-                />
-                <span>
-                  <i className="ri-search-line"></i>
-                </span>
-              </div>
-            </Col>
-            <Col lg="6" md="6" sm="6" xs="12" className="mb-4">
-              <div className="sorting__widget text-end">
-                <select className="w-50">
-                  <option>Default</option>
-                  <option value="high-price">High price</option>
-                  <option value="low-price">Low price</option>
-                </select>
-              </div>
-            </Col>
-
-            {displayPage.map((item) => (
-              <Col lg="3" md="4" sm="6" xs="6" key={item.id} className="mb-4">
-                <ProductCard item={item} />
+            <div className="">
+              <Col lg="3" md="4" sm="6" xs="6" className="mb-4 ">
+                <ProductCard product={products} key={products.id} />
               </Col>
-            ))}
-            <div>
-              <ReactPaginate
-                pageCount={pageCount}
-                onPageChange={changePage}
-                previousLabel="Prev"
-                nextLabel="Next"
-                containerClassName="paginationBtns"
-              />
             </div>
           </Row>
         </Container>
